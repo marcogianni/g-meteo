bc28358b1d21d48a80e00d7af4103240f38a97fc
Marco Gianni <gian93ma50@gmail.com>
2023-10-21 11:15:53 +0200

    added debugger

--- app/ReactQueryProvider.tsx
+++ app/ReactQueryProvider.tsx
@@ -2,16 +2,21 @@
 
 import { useState, ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 type Props = {
   children: JSX.Element | JSX.Element[] | string | string[] | ReactNode;
+  debug?: boolean;
 };
 
 export default function ReactQueryProvider(props: Props) {
-  const { children } = props;
+  const { children, debug = false } = props;
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {debug && <ReactQueryDevtools initialIsOpen={true} />}
+      {children}
+    </QueryClientProvider>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           // enableSystem
           disableTransitionOnChange
         >
-          <ReactQueryProvider>
+          <ReactQueryProvider debug={false}>
             <Navbar />
             {children}
           </ReactQueryProvider>
